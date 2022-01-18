07156669181e3ebb340093f8114c45e6cc942cd4
vvishal11 <11kumar.vishal4@gmail.com>
2022-01-18 22:52:52 +0530

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -7,9 +7,9 @@ export default function Main() {
       <div class="container px-5 pt-32 mx-auto lg:px-4 lg:py-4">
         <div class="flex flex-col w-full mb-2 text-left md:text-center ">
           <h1 class="mb-2 text-6xl font-bold tracking-tighter text-white lg:text-8xl md:text-7xl">
-            <span>We are making </span>
+            <span>We are one of a </span>
             <br class="hidden lg:block"></br>
-            Stunning websites
+            kind community backed venture
           </h1>
           <br></br>
           <p class="mx-auto  text-xl font-normal leading-relaxed text-gray-600 dark:text-gray-300 lg:w-2/3">
