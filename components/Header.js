a7766e2949ea5b1552eafc419fcaf8aa93321a0d
vvishal11 <11kumar.vishal4@gmail.com>
2022-01-19 15:49:02 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -48,9 +48,10 @@ export default function Header() {
        <button class="px-4 py-2 mt-2 text-sm font-semibold text-white bg-blue-600 transition duration-500 ease-in-out transform bg-transparent rounded-lg dark:text-gray-300 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
           Download
        </button>
-          */}
+         
 
         </div>
+*/}
       </div>
     </header>
   );
