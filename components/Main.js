fcd1f557dbee1586fbcb0089dcf2ade95acad148
vvishal11 <11kumar.vishal4@gmail.com>
2022-01-19 20:38:07 +0530

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -216,7 +216,7 @@ export default function Main() {
                   <div class="h-full flex flex-col items-center text-center">
                     <img
                       alt="team"
-                      class="flex-shrink-0 rounded-full w-full h-full object-cover object-center mb-4 "
+                      class="flex-shrink-0 rounded-full w-full h-56 object-cover object-center mb-4 "
                       src="./images/M01.png"
                     ></img>
                     <div class="w-full">
