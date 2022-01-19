4cc54929cfaae9ed66f8172f6b2fec30ba524b7e
vvishal11 <11kumar.vishal4@gmail.com>
2022-01-19 16:31:18 +0530

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -19,7 +19,7 @@ export default function Main() {
       </div>
       <div class="container flex flex-col items-center justify-center py-8 mx-auto rounded-lg md:p-1 p-3">
         <img
-          class="object-cover object-center w-full mb-10 g327 border rounded-lg shadow-md"
+          class="object-cover object-center w-full mb-10 g327 border rounded-lg"
           alt="hero"
           src="./images/placeholder03.png"
         ></img>
