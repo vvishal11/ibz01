771290df1697acec11c124044174eb05b1e84986
vvishal11 <11kumar.vishal4@gmail.com>
2022-01-22 15:31:18 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,6 +6,8 @@ export default function Footer() {
           <span class="ml-3 text-xl">infinityuponzero</span>
         </a>
         <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+    
+    {/*
           <a href="https://facebook.com/">
             <svg
               fill="currentColor"
@@ -18,6 +20,7 @@ export default function Footer() {
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
+   */ } 
           <a class="ml-3" href="https://twitter.com/infinityupon0">
             <svg
               fill="currentColor"
@@ -37,11 +40,12 @@ export default function Footer() {
     
     <svg xmlns="http://www.w3.org/2000/svg" 
     fill="currentColor"
+width="16" height="16"
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
               class="w-5 h-5"
-              viewBox="0 0 24 24"
+              viewBox="0 0 16 16"
     >
     
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
@@ -49,8 +53,16 @@ export default function Footer() {
     
     
           </a>
+
+       <a class="ml-3" href="mailto:iuponzero@gmail.com">
+       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open" viewBox="0 0 16 16">
+  <path d="M8.47 1.318a1 1 0 0 0-.94 0l-6 3.2A1 1 0 0 0 1 5.4v.817l5.75 3.45L8 8.917l1.25.75L15 6.217V5.4a1 1 0 0 0-.53-.882l-6-3.2ZM15 7.383l-4.778 2.867L15 13.117V7.383Zm-.035 6.88L8 10.082l-6.965 4.18A1 1 0 0 0 2 15h12a1 1 0 0 0 .965-.738ZM1 13.116l4.778-2.867L1 7.383v5.734ZM7.059.435a2 2 0 0 1 1.882 0l6 3.2A2 2 0 0 1 16 5.4V14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5.4a2 2 0 0 1 1.059-1.765l6-3.2Z"/>
+</svg>
+          </a>
+
+
  
-    
+    {/*
           <a class="ml-3" href="https://instagram.com/">
             <svg
               fill="none"
@@ -65,7 +77,7 @@ export default function Footer() {
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
-    {/*
+    
           <a class="ml-3" href="https://www.linkedin.com/in/">
             <svg
               fill="currentColor"
