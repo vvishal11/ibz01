795e3e762744c26b429f9b3f54a18105f5f6e836
vvishal11 <11kumar.vishal4@gmail.com>
2022-01-25 18:39:55 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -35,7 +35,7 @@ export default function Footer() {
           </a>
     
     
-              <a class="ml-3" href="https://t.me/IndiaDefi">
+              <a class="ml-3" href="https://t.me/CryptoGamingGems">
             
     
     <svg xmlns="http://www.w3.org/2000/svg" 
